d797d407652a99f13e2d5fff538321560ad97aba
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-08-03 23:52:39 +0530

    fix: change images of hostels

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -3,6 +3,9 @@ import SRILANKA_IMG from "../img/Srilanka.jpg";
 import VARKALA_IMG_1 from "../img/varkala1.jpg";
 import VARKALA_IMG_2 from "../img/varkala2.png";
 import BIR from "../img/bir.jpg";
+import weligamaimg from "../img/weligama.jpg";
+import hiriketiyaimg from "../img/hirektya.jpg";
+import comingsoonimg from "../img/COMING SOON.png";
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -36,7 +39,11 @@ const Locations = () => {
         className="mySwiper locationslider"
       >
         <SwiperSlide className="relative">
-          <img src={SRILANKA_IMG} alt="goa" className="rounded-md shadow-lg" />
+          <img
+            src={weligamaimg}
+            alt="weligamaimg"
+            className="rounded-md shadow-lg"
+          />
           <div className="bg-white absolute top-[50%] w-[80%] rounded-md p-5 mx-10 text-left shadow-md">
             <h3 className=" text-[#116b89] font-playfair font-semibold text-lg mb-1">
               Weligama
@@ -52,15 +59,19 @@ const Locations = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={GOA_IMG} alt="goa" className="rounded-md shadow-lg" />
+          <img
+            src={hiriketiyaimg}
+            alt="hiriketiyaimg"
+            className="rounded-md shadow-lg"
+          />
           <div className="bg-white absolute top-[50%] w-[80%] rounded-md p-5 mx-10 text-left shadow-md">
             <h3 className=" text-[#116b89] font-playfair font-semibold text-lg mb-1">
               Hiriketiya
             </h3>
             <p className="text-sm text-[#a9a9a9]">
-              In the lap of South Goa, The Lost Hostel is a budget-friendly
-              accommodation offering a garden, indoor games and shared
-              dormitories.
+              50m from the bustling surfers paradise of Weligama beach, the
+              hostel offers peaceful accommodation with a garden, yoga area,
+              indoor games and shared dorms
             </p>
             <a
               href="https://live.ipms247.com/booking/book-rooms-thelosthostelsgoapalolembeach"
@@ -73,24 +84,11 @@ const Locations = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={VARKALA_IMG_1} alt="goa" className="rounded-md shadow-lg" />
+          <img src={comingsoonimg} alt="comingsoonimg" className="rounded-md shadow-lg" />
           <div className="bg-white absolute top-[50%] w-[80%] rounded-md p-5 mx-10 text-left shadow-md">
             <h3 className=" text-[#116b89] font-playfair font-semibold text-lg mb-1">
               Sri Lanka
             </h3>
-            <p className="text-sm text-[#a9a9a9]">
-              Located along the famous cliffs and beaches of Varkala, The Lost
-              Hostel offers enthralling stays complete with exciting outdoor and
-              indoor activities.
-            </p>
-            <a
-              href="https://live.ipms247.com/booking/book-rooms-thelosthostelvarkalahelipad"
-              target="_blank"
-            >
-              <button className="font-medium font-playfair text-sm text-[#116b89] underline decoration-[1.5px]">
-                Book Now
-              </button>
-            </a>
           </div>
         </SwiperSlide>
         <SwiperSlide>
@@ -129,7 +127,7 @@ const Locations = () => {
           <img src={GOA_IMG} alt="goa" className="rounded-md shadow-lg" />
           <div className="bg-white absolute top-[50%] w-[80%] rounded-md p-5 mx-10 text-left shadow-md">
             <h3 className=" text-[#116b89] font-playfair font-semibold text-lg mb-1">
-              South Goa
+              South Goa - Palolem
             </h3>
             <p className="text-sm text-[#a9a9a9]">
               In the lap of South Goa, The Lost Hostel is a budget-friendly
@@ -150,7 +148,7 @@ const Locations = () => {
           <img src={VARKALA_IMG_1} alt="goa" className="rounded-md shadow-lg" />
           <div className="bg-white absolute top-[50%] w-[80%] rounded-md p-5 mx-10 text-left shadow-md">
             <h3 className=" text-[#116b89] font-playfair font-semibold text-lg mb-1">
-              Varkala
+              Varkala - North Cliff
             </h3>
             <p className="text-sm text-[#a9a9a9]">
               Located along the famous cliffs and beaches of Varkala, The Lost
@@ -171,7 +169,7 @@ const Locations = () => {
           <img src={BIR} alt="goa" className="rounded-md shadow-lg" />
           <div className="bg-white absolute top-[50%] w-[80%] rounded-md p-5 mx-10 text-left shadow-md">
             <h3 className=" text-[#116b89] font-playfair font-semibold text-lg mb-1">
-              Bir
+              Bir - Landing Site
             </h3>
             <p className="text-sm text-[#a9a9a9]">
               Located near the Paragliding landing site, The Lost Hostels Bir
