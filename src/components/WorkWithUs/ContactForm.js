39057ae01146d833f4341ad42287083fffc3fd2d
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-08-12 19:16:12 +0530

    feat: add contact form that sends email

--- src/components/WorkWithUs/ContactForm.js
+++ src/components/WorkWithUs/ContactForm.js
@@ -1,58 +1,56 @@
 import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 
 const ContactForm = () => {
+  const [state, handleSubmit] = useForm("mpzgpjzn");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
-    <section className="py-16 px-8 bg-white text-center" data-aos="fade-up" id="contactus">
+    <section
+      className="py-16 px-8 bg-white text-center flex flex-col justify-center items-center"
+      data-aos="fade-up"
+      id="contactus"
+    >
       <h2 className="text-3xl font-playfair font-semibold py-4 text-[#116b89]">
         Contact Us
       </h2>
-      <form className="max-w-md mx-auto">
-        <div className="mb-4">
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="message"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Message
-          </label>
-          <textarea
-            id="message"
-            name="message"
-            rows="4"
-            className="mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-            required
-          ></textarea>
-        </div>
-        <button className="border-2 border-[#116b89] p-2 px-5 my-4 rounded-md font-medium font-dmsans text-sm text-[#116b89]">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col  text-[#4c4e4e] w-1/3 text-left"
+      >
+        <label htmlFor="name" className="text-sm">Name : </label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          className="border-2 border-[#f4f4f4] rounded p-1 my-1"
+        />
+        <ValidationError prefix="name" field="name" errors={state.errors} />
+        <label htmlFor="email" className="text-sm">Email Address : </label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          className="border-2 border-[#f4f4f4] rounded p-1 my-1"
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <label htmlFor="message" className="text-sm">Message : </label>
+        <textarea
+          id="message"
+          name="message"
+          className="border-2 border-[#f4f4f4] rounded p-1 my-1"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+        <button
+          type="submit"
+          disabled={state.submitting}
+          className="border-2 bg-[#116b89] p-2 px-5 my-4 rounded-md font-medium font-dmsans text-sm text-[#fff]"
+        >
           Submit
         </button>
       </form>
