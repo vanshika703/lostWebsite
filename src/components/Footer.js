2e3c7797db69efe478f0409e5cffa304df651a85
Harsh Goel <harshgoel05@gmail.com>
2023-08-15 19:25:16 +0530

    add temporary links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,11 @@ const Footer = () => {
             The Lost Hostels
           </h1>
           <p className="text-xl text-[#8cb6c4]">Come get lost with us.</p>
-          <button className="border-2 border-[#8cb6c4] p-1 px-3 my-4 rounded-md font-medium font-dmsans">
-            Book Now
-          </button>
+          <Link to="/">
+            <button className="border-2 border-[#8cb6c4] p-1 px-3 my-4 rounded-md font-medium font-dmsans">
+              Book Now
+            </button>
+          </Link>
         </div>
         <div
           className="flex flex-col sm:items-end"
@@ -27,14 +29,10 @@ const Footer = () => {
               <li className="text-sm text-[#8cb6c4]">Privacy Policy</li>
             </Link>
             <Link to="/guest">
-              <li className="text-sm text-[#8cb6c4]">
-                Guest Policy
-              </li>
+              <li className="text-sm text-[#8cb6c4]">Guest Policy</li>
             </Link>
             <Link to="/cancellation">
-              <li className="text-sm text-[#8cb6c4]">
-                Cancellation Policy
-              </li>
+              <li className="text-sm text-[#8cb6c4]">Cancellation Policy</li>
             </Link>
             <Link to="/tourgroups">
               <li className="text-sm text-[#8cb6c4]">
@@ -67,8 +65,12 @@ const Footer = () => {
             <li className="text-sm text-[#8cb6c4]">Varkala: +91 7012416343</li>
             <li className="text-sm text-[#8cb6c4]">Goa: +91 9307762867</li>
             <li className="text-sm text-[#8cb6c4]">Bir: +91 6230267754</li>
-            <li className="text-sm text-[#8cb6c4]">Weligama: +94 77 834 0685</li>
-            <li className="text-sm text-[#8cb6c4]">Hiriketiya: +94 74 220 4478</li>
+            <li className="text-sm text-[#8cb6c4]">
+              Weligama: +94 77 834 0685
+            </li>
+            <li className="text-sm text-[#8cb6c4]">
+              Hiriketiya: +94 74 220 4478
+            </li>
             <div class="flex items-center mt-2">
               <a
                 rel="noopener noreferrer"
@@ -110,10 +112,45 @@ const Footer = () => {
                   <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
                 </svg>
               </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://www.facebook.com/losthostelgoa/"
+                target="_blank"
+                class="flex justify-center items-center h-8 w-8 group relative border-2 border-[#8cb6c4] rounded-full mr-3"
+              >
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 320 512"
+                  class="absolute group-hover:text-logoGreen left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer"
+                  className="h-3 w-3"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
+                </svg>
+              </a>
+              <a
+                rel="noopener noreferrer"
+                href="https://www.facebook.com/losthostelgoa/"
+                target="_blank"
+                class="flex justify-center items-center h-8 w-8 group relative border-2 border-[#8cb6c4] rounded-full mr-3"
+              >
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 320 512"
+                  class="absolute group-hover:text-logoGreen left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer"
+                  className="h-3 w-3"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
+                </svg>
+              </a>
             </div>
           </ul>
         </div>
-        
       </div>
       <div className="bg-[#094256] flex flex-row  justify-between px-10 sm:px-28 py-2 text-[#3e7a8e] text-xs items-center">
         <p>&#169; 2023 Lost Hostels</p>
