5d1fe291b7bf59a36ac0e68d841e0058014c400b
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-08-21 10:56:01 +0530

    feat: error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -8,7 +8,7 @@ const Error = () => {
       <h1 className="text-3xl font-playfair font-semibold py-4 text-[#116b89]">
         Error : 404
       </h1>
-      <img src={errorimg} alt="error" className="w-1/3 m-5 "/>
+      <img src={errorimg} alt="error" className="sm:w-1/3 m-5 "/>
       <p>You seem to be lost.</p>
       <p>Go back to <HashLink to="/" className="underline">Home</HashLink>.</p>
     </div>
