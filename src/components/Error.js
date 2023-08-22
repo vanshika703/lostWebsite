bf3f647dc06f80e61367c8a1cee9a464cd0080e8
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-08-22 16:42:30 +0530

    feat: change error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,17 +1,28 @@
 import React from "react";
-import errorimg from "../img/error.svg"
+import errorimg from "../img/error.svg";
 import { HashLink } from "react-router-hash-link";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Error = () => {
   return (
-    <div className="flex flex-col justify-center items-center m-10 p-10">
-      <h1 className="text-3xl font-playfair font-semibold py-4 text-[#116b89]">
-        Error : 404
-      </h1>
-      <img src={errorimg} alt="error" className="sm:w-1/3 m-5 "/>
-      <p>You seem to be lost.</p>
-      <p>Go back to <HashLink to="/" className="underline">Home</HashLink>.</p>
-    </div>
+    <>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center m-10 p-10">
+        <h1 className="text-3xl font-playfair font-semibold py-4 text-[#116b89]">
+          Great things are coming!
+        </h1>
+        <img src={errorimg} alt="error" className="sm:w-1/3 m-5 " />
+        <p>We are working hard so you do not have to see this page in the future.</p>
+        <p>
+          In the meanwhile, chill at one of our hostels🌊
+          <HashLink to="/" className="underline">
+            Book Here
+          </HashLink>
+        </p>
+      </div>
+      <Footer />
+    </>
   );
 };
 
