3c6491ba4badc940574ab8a523e8d1e1ab4d7252
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-08-22 23:59:26 +0530

    feat:add new images

--- src/components/HostelDetails/Gallery.js
+++ src/components/HostelDetails/Gallery.js
@@ -1,28 +1,16 @@
 
 const Gallery = ({ hostelData }) => {
   return (
-    <div className="sm:h-[90vh] flex flex-col sm:flex-row justify-center mx-5 sm:mx-28 mt-[30vh] sm:mt-[10vh]">
-      <img
-        src={hostelData?.photo1}
-        className="sm:w-2/3 sm:h-full p-2 object-cover"
-        alt="hostel"
-      ></img>
-      <div className="sm:w-1/3 h-full">
-        <img
-          src={hostelData?.photo2}
-          className="w-full h-1/3 p-2 object-cover"
-          alt="hostel"
-        ></img>
-        <img
-          src={hostelData?.photo3}
-          className="w-full h-1/3  p-2 object-cover"
-          alt="hostel"
-        ></img>
-        <img
-          src={hostelData?.photo4}
-          className="w-full h-1/3  p-2 object-cover"
-          alt="hostel"
-        ></img>
+    <div className="sm:h-[90vh] flex flex-col justify-center mx-5 sm:mx-28 mt-[30vh] sm:mt-[10vh]">
+      <div className="h-1/2 w-full flex">
+        <img src={hostelData?.photo1} className="w-1/2 p-1 object-cover"/>
+        <img src={hostelData?.photo2} className="w-1/2 p-1 object-cover" />
+
+      </div>
+      <div className="h-1/2 w-full flex">
+        <img src={hostelData?.photo3} className="w-1/3 p-1 object-cover"/>
+        <img src={hostelData?.photo4} className="w-1/3 p-1 object-cover" />
+        <img src={hostelData?.photo5} className="w-1/3 p-1 object-cover" />
       </div>
     </div>
   );
