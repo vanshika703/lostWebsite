ab85dfc2d73737bd0a1594b276ec942e7215825f
Harsh Goel <harshgoel05@gmail.com>
2023-08-22 14:13:26 +0530

    fix: follow button

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,7 +3,7 @@ const Gallery = () => {
     <section id="gallery" className="my-20 w-[100vw] relative">
       <div data-mc-src="e5d04892-6959-4e57-ab88-235f4c8eaf24#instagram"></div>
       <div
-        className="bg-white absolute w-[100vw] h-32 -bottom-4  z-50"
+        className="bg-white absolute w-[100vw] h-10 sm:h-12 lg:h-10 -bottom-4  z-50"
         onClick={() => {}}
       ></div>
     </section>
