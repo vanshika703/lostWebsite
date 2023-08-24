18b42c813fb073e8099d858e8ff4686946b62db2
Vanshika <vanshikabhojnagarwala@gmail.com>
2023-08-24 14:00:36 +0530

    date

--- src/components/Date.js
+++ src/components/Date.js
@@ -14,7 +14,7 @@ const DateInput = ({ onChange }) => {
       type="date"
       min={new Date().toISOString().split("T")[0]}
       value={value}
-      className="text-black bg-transparent"
+      className="text-white bg-transparent"
       onChange={onChangeDate}
       placeholder="Date"
     />
