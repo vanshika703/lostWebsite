cbd5e0f14b034ea0ab8e564c971b434e26773fe4
Harsh Goel <harshgoel05@gmail.com>
2023-08-24 23:03:06 +0530

    fix: gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,6 +1,6 @@
 const Gallery = () => {
   return (
-    <section id="gallery" className="my-20 w-[100vw] relative">
+    <section id="gallery" className="my-20 relative px-5">
       <div data-mc-src="e5d04892-6959-4e57-ab88-235f4c8eaf24#instagram"></div>
       <div
         className="bg-white absolute w-[100vw] h-10 sm:h-12 lg:h-10 -bottom-4  z-50"
